c88338533fe1fcdc2024aaddffa8208f48e0cf1d
gioezzy <giovanniyuda29@gmail.com>
2026-01-09 02:27:21 +0700

    feat: Auth Routes

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    
+    Route::post('/orders', [\App\Http\Controllers\OrderController::class, 'store']);
+    Route::get('/orders', [\App\Http\Controllers\OrderController::class, 'index']);
+});
+
+Route::get('/products', [\App\Http\Controllers\ProductItemController::class, 'index']);
+Route::get('/products/{id}', [\App\Http\Controllers\ProductItemController::class, 'show']);
