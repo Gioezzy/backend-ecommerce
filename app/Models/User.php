8352ac18dc73b5f39de420cde9201918713c4882
gioezzy <giovanniyuda29@gmail.com>
2026-01-09 18:14:17 +0700

    fix: Restore missing JWTSubject import

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
@@ -73,8 +74,6 @@ class User extends Authenticatable implements JWTSubject, FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        // Allow all users for now, or you can restrict it:
-        // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
         return true;
     }
 }
